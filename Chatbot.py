df3f879367e26032cdb5ed8f03a6b5e40f8c88a4
Rithwik-180613 <ssrithwik@gmail.com>
2026-05-06 22:03:33 +0530

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -1,13 +1,13 @@
 print("Hi I am your Chatbot")
-print("Typr bye to stop chatting")
-while True:
-    x=input("Enter your Prompt").lower()
+print("Type bye to stop chatting")
+x=input("Enter your Prompt").lower()
+while x!='bye':
     if(x=='hi'):
         print("Hello,How can I help you!")
     elif(x=='how are you'):
         print('I am doing Good')
     elif(x=='how are you feeling now'):
         print('I am Happy')
-    else:
-        print("Bye !!! Have a nice day")
-        break           
+    x=input("Enter your Prompt").lower()    
+print("Bye !!! Have a nice day")
+                  
